439b7d938116ed7cacd67dbc62bb6691b42b526a
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2017-06-24 15:42:48 -0600

    cleanup

--- TwitchLib/TwitchApp.cs
+++ TwitchLib/TwitchApp.cs
@@ -9,15 +9,18 @@ namespace TwitchLib
 {
     public class TwitchApp
     {
-        private string code;
         private OAuth oauth;
         private Fuel fuel;
-        public TwitchApp(string code)
+        public TwitchApp()
         {
             this.code = code;
         }
 
-        public async void Login()
+        /// <summary>
+        /// Calls two authorization calls to create OAuth and Fuel models which are used to access the rest of the curse service.
+        /// </summary>
+        /// <param name="code">This code comes from here: https://api.twitch.tv/kraken/oauth2/authorize?action=callback&client_id=jf3xu125ejjjt5cl4osdjci6oz6p93r&redirect_uri=https%3A%2F%2Fweb.curseapp.net%2Flaguna%2Fpassport-callback.html&response_type=code Go there, login, and after being redirected, look at the new URL in the browser and copy the code parameter.</param>
+        public async void Login(string code)
         {
             oauth = await Internal.Requests.TwitchApp.Auth(code);
             fuel = await Internal.Requests.TwitchApp.Fuel(oauth.Session.Token);
